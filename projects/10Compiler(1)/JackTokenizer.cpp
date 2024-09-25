ddccb9d5f5455d50791d17841f789f05ac4b0a8e
Angaj Sharma <trancidence@gmail.com>
2024-09-25 14:08:18 +0530

    Worked out the next token core functionality

--- projects/10Compiler(1)/JackTokenizer.cpp
+++ projects/10Compiler(1)/JackTokenizer.cpp
@@ -2,6 +2,13 @@
 #include <fstream>
 #include <sstream>
 
+namespace // private functions declaration
+{
+    bool isSymbol(char c);
+    bool isKeyword(char keyword[]);
+    bool isIdentifier(char id[]);
+}
+
 analyzer::JackTokenizer::JackTokenizer(std::stringstream &path)
 { // opens a file(s) and dumps the data in a string
     std::string fileName;
@@ -31,7 +38,7 @@ bool analyzer::JackTokenizer::hasMoreTokens()
     {
         // std::cout << "ran" << std::endl;
 
-        if (*fp == '/' && *(fp + 1) == '/')
+        if (*fp == '/' && *(fp + 1) == '/') // ignore line comments
         {
             while (*fp != '\n')
                 fp++;
@@ -39,7 +46,7 @@ bool analyzer::JackTokenizer::hasMoreTokens()
             continue;
         }
 
-        else if (*fp == '/' && *(fp + 1) == '*' && *(fp + 2) == '*')
+        else if (*fp == '/' && *(fp + 1) == '*' && *(fp + 2) == '*') // ignore multi-line commends
         {
             fp = fp + 4;
             while (*fp != '/' && *(fp - 1) != '*')
@@ -57,17 +64,47 @@ bool analyzer::JackTokenizer::hasMoreTokens()
             break;
         }
 
-        if (*fp == '\n' || *fp == '\r')
+        if (*fp == '\n' || *fp == '\r') // ignore line breaks
         {
             *fp++;
             continue;
         }
 
-        if (*fp == ' ')
+        if (*fp == ' ') // ignore whitespace
         {
             fp++;
             continue;
         }
+
+        else if (isSymbol(*fp)) // if symbol
+        {
+            *wp++ = *fp++;
+            break;
+        }
+
+        else if (isdigit(*fp)) // if integerConstant
+        {
+            do
+            {
+                *wp++ = *fp++;
+            } while (isdigit(*fp));
+            break;
+        }
+
+        do // grab a string
+        {
+            *wp++ = *fp++;
+        } while (*fp != ' ');
+
+        if (isKeyword(current_token))
+            break;
+        if (isIdentifier(current_token))
+            break;
+
+        else
+            std::cout << "Error processing token " << current_token << "or" << *fp << std::endl;
+        return 1;
+
         fp++; // if none
 
     } while (*fp);
@@ -75,4 +112,32 @@ bool analyzer::JackTokenizer::hasMoreTokens()
     std::cout << ":" << current_token << ":" << std::endl;
 
     return 0;
+}
+
+namespace
+{
+
+    bool isSymbol(char c)
+    {
+        if (c == '{' || c == '}' || c == '(' || c == ')' || c == '[' || c == '.' || c == ',' || c == ';' || c == '+' || c == '-' || c == '*' || c == '/' || c == '&' || c == '|' || c == '|' || c == '<' || c == '>' || c == '=' || c == '~')
+            return true;
+        else
+            return false;
+    }
+
+    bool isKeyword(char keyword[])
+    {
+        if (keyword == "class" || keyword == "constructor" || keyword == "function" || keyword == "method" || keyword == "field" || keyword == "static" || keyword == "var" || keyword == "int" || keyword == "char" || keyword == "boolean" || keyword == "void" || keyword == "true" || keyword == "false" || keyword == "null" || keyword == "this" || keyword == "let" || keyword == "do" || keyword == "if" || keyword == "else" || keyword == "while" || keyword == "return")
+            return true;
+        else
+            return false;
+    }
+
+    bool isIdentifier(char id[])
+    {
+        if (isdigit(id[0]))
+            return false;
+        else
+            return true;
+    }
 }
