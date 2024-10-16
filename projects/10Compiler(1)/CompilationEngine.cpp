db5f0d8e476f9d5fe5ef4d48c43f873429524d7f
Angaj Sharma <trancidence@gmail.com>
2024-10-16 14:56:10 +0530

    Defined and tested helper functions

--- projects/10Compiler(1)/CompilationEngine.cpp
+++ projects/10Compiler(1)/CompilationEngine.cpp
@@ -1,7 +1,13 @@
 #include "JackAnalyzer.h"
 #include <fstream>
 
-// inhertiance not vs share object as private member
+namespace
+{
+    int indentLevel = 0;
+    void writeData(const std::string &type, std::string data, std::string angleString, std::fstream &handle);
+    void indent(std::fstream &handle, bool indent);
+
+} // namespace
 
 analyzer::CompilationEngine::CompilationEngine(std::stringstream &path) : token(path)
 {
@@ -10,21 +16,62 @@ analyzer::CompilationEngine::CompilationEngine(std::stringstream &path) : token(
     filename.resize(filename.find('.'));
     filehandle.open(filename + "_c" + ".xml", std::ofstream::out | std::ofstream::trunc);
 
-    filehandle << "<token>\n";
     CompileClass();
-
-    std::cout << "here";
 }
 
 void analyzer::CompilationEngine::CompileClass()
 {
+#if 0
     while (token.hasMoreTokens())
-        std::cout << token.current_token << std::endl;
+        std::cout << token.current_token << '#' << std::endl;
+    std::cout << token.current_token;
+#endif
+    if (token.hasMoreTokens())
+    {
+        writeData("angled", " ", token.current_token, filehandle);
+        indent(filehandle, 1);
+        writeData("angled-data", " name of class ", token.current_token, filehandle);
+        indent(filehandle, 0);
+        writeData("close", " name of class ", token.current_token, filehandle);
+    }
 }
 
 void analyzer::CompilationEngine::Close(bool flag)
 {
 
-    filehandle << "</token>";
     filehandle.close();
+}
+
+namespace
+{
+    void writeData(const std::string &type, std::string data, std::string angleString, std::fstream &handle)
+    {
+        if (type == "angled")
+            handle << '<' + angleString + ">\n";
+
+        else if (type == "angled-data")
+        {
+            handle << '<' + angleString + ">";
+            handle << ' ' + data + ' ';
+            handle << '<' + angleString + ">\n";
+        }
+        else if (type == "close")
+            handle << "</" + angleString + ">\n";
+
+        else
+            handle << data + '\n';
+    }
+
+    void indent(std::fstream &handle, bool indent)
+    {
+        if (indent)
+            indentLevel++;
+        else
+            indentLevel--;
+
+        for (int i = indentLevel; i > 0; i--)
+        {
+            handle << '\t';
+        }
+    }
 }
